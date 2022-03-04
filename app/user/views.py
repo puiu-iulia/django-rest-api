755a29f25f5b4a33c37288a2696fa9d2c3356f8a
Iulia Puiu <iulia.puiu@hotmail.com>
2022-03-04 16:01:46 +0200

    Create update user endpoint - part 1

--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from app.user import serializers
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from user.serializers import UserSerializer, AuthTokenSerializer
@@ -13,3 +14,14 @@ class CreateTokenView(ObtainAuthToken):
     '''Create a new auth token for user'''
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    '''Manage/update the authenticated user'''
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permissions_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        '''Retrieve and return authenticated user'''
+        return self.request.user
