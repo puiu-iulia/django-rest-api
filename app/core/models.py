de2db6fc4f62d87046a5ac023d826923caf90b53
Iulia Puiu <iulia.puiu@hotmail.com>
2022-03-04 10:55:48 +0200

    Custom admin - finished

--- app/core/models.py
+++ app/core/models.py
@@ -31,7 +31,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     '''Custom user model that supports using email instead of username'''
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    isActive = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -25,3 +25,18 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        '''Test that the user edit page works'''
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        # /admin/core/user/id
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        '''Test that the create user page works'''
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
